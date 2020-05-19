b68d9dba5008d0e58bbfac454bee14f665f0a785
Alireza Tavakoli <44779572+alirezatwk@users.noreply.github.com>
2020-05-19 18:52:20 +0430

    Verilog Codes Added

---
+++ Controller.v
@@ -0,0 +1,187 @@
+`timescale 1ns/1ns
+
+module Controller (input zeroflag, input [31:0]instruction, input clk, rst, output reg PCWrite, PCWriteCond, IorD, MemWrite, MemRead, IRWrite, RegDst, WriteRegSel, MemtoReg, WriteDataSel, RegWrite, ALUSrcA, output reg [1:0]ALUSrcB, PCSrc, output reg [2:0]ALUoperation);
+    parameter [3:0] IF = 4'b0000, ID = 4'b0001, JumpComplete = 4'b0010, branchComplete = 4'b0011, RTstart = 4'b0100, RTcomplete = 4'b0101, MemRefStart = 4'b0110, SWcomplete = 4'b0111, LWstart = 4'b1000, LWcomplete = 4'b1001, JumpRcomplete = 4'b1010, JALcomplete = 4'b1011, RTcomplete3 = 4'b1100;
+
+    reg [3:0]ps, ns;
+    wire [5:0]functionType, Opcode;
+    assign Opcode = instruction[31:26];
+    assign functionType = instruction[5:0];
+    always @(zeroflag, instruction, ps)begin
+        {PCWrite, PCWriteCond, IorD, MemWrite, MemRead, IRWrite, RegDst, WriteRegSel, MemtoReg, WriteDataSel, RegWrite, ALUSrcA} = 12'b0;
+        ALUSrcB = 2'b00;
+        PCSrc = 2'b00;
+        ALUoperation = 3'b000;
+        ns = IF;
+        case(ps)
+            IF : begin
+                IorD = 1'b0;
+                MemRead = 1'b1;
+                IRWrite = 1'b1;
+                ALUSrcA = 1'b0;
+                ALUSrcB = 2'b01;
+                ALUoperation = 3'b010; // Add command
+                PCSrc = 2'b00;
+                PCWrite = 1'b1;
+                ns = ID;
+            end
+            ID : begin
+                ALUSrcA = 1'b0;
+                ALUSrcB = 2'b11;
+                ALUoperation = 3'b010; // Add command
+                case(Opcode)
+                    6'b000000 : begin //.. case R-type
+                        ns = RTstart;
+                    end
+                    6'b000010 : begin //.. case j
+                        ns = JumpComplete;
+                    end
+                    6'b000011 : begin //.. case JAL
+                        ns = JALcomplete;
+                    end
+                    6'b000100 : begin //.. case beq
+                        ns = branchComplete;
+                    end
+                    6'b000101 : begin //.. case bne
+                        ns = branchComplete;
+                    end
+                    6'b001000 : begin //.. case addi
+                        ns = RTstart;
+                    end
+                    6'b001100 : begin //.. case andi
+                        ns = RTstart;
+                    end
+                    6'b100011 : begin //.. case lw
+                        ns = MemRefStart;
+                    end
+                    6'b101011 : begin //.. case sw
+                        ns = MemRefStart;
+                    end
+                    6'b100000 : begin //.. case jr
+                        ns = JumpRcomplete;
+                    end
+                endcase
+            end
+            JumpComplete : begin
+                PCSrc = 2'b01;
+                PCWrite = 1'b1;
+                ns = IF;
+            end
+            branchComplete : begin
+                ALUSrcA = 1'b1;
+                ALUSrcB = 2'b00;
+                ALUoperation = 3'b110; //.. subtract command
+                PCWriteCond = 1'b1;
+                PCSrc = 2'b10;
+                ns = IF;
+            end
+            RTstart : begin
+                case(Opcode)
+                    6'b000000 : begin //.. ordinary rtype
+                        ALUSrcA = 1'b1;
+                        ALUSrcB = 2'b00;
+                        ns = RTcomplete3;
+                        case(functionType)
+                            6'b100000 : begin //.. case add function
+                                ALUoperation = 3'b010;
+                            end
+                            6'b100010 : begin //.. case sub function
+                                ALUoperation = 3'b110;
+                            end
+                            6'b100100 : begin //.. case and function 
+                                ALUoperation = 3'b000;
+                            end
+                            6'b100101 : begin //.. case or function
+                                ALUoperation = 3'b001;
+                            end
+                            6'b101010 : begin //.. case slt function
+                                ALUoperation = 3'b111;
+                            end
+                        endcase
+                    end
+                    6'b001000 : begin //.. addi
+                        ALUSrcA = 1'b1;
+                        ALUSrcB = 2'b10;
+                        ALUoperation = 3'b010;
+                        ns = RTcomplete;
+                    end
+                    6'b001100 : begin //.. andi
+                        ALUSrcA = 1'b1;
+                        ALUSrcB = 2'b10;
+                        ALUoperation = 3'b000;
+                        ns = RTcomplete;
+                    end
+                endcase
+            end
+            RTcomplete : begin
+                RegDst = 1'b0;
+                WriteRegSel = 1'b0;
+                MemtoReg = 1'b0;
+                WriteDataSel = 1'b0;
+                RegWrite = 1'b1;
+                ns = IF;
+            end
+            RTcomplete3 : begin
+                RegDst = 1'b1;
+                WriteRegSel = 1'b0;
+                MemtoReg = 1'b0;
+                WriteDataSel = 1'b0;
+                RegWrite = 1'b1;
+                ns = IF;
+            end
+            MemRefStart : begin
+                ALUSrcA = 1'b1;
+                ALUSrcB = 2'b10;
+                ALUoperation = 3'b010; //.. add command
+                case(Opcode)
+                    6'b100011 : begin //.. case lw
+                        ns = LWstart;
+                    end
+                    6'b101011 : begin //.. case sw
+                        ns = SWcomplete;
+                    end
+                endcase
+            end
+            SWcomplete : begin
+                IorD = 1'b1;
+                MemWrite = 1'b1;
+                ns = IF;
+            end
+            LWstart : begin
+                IorD = 1'b1;
+                MemRead = 1'b1;
+                ns = LWcomplete;
+            end
+            LWcomplete : begin
+                RegDst = 1'b0;
+                WriteRegSel = 1'b0;
+                MemtoReg = 1'b1;
+                WriteDataSel = 1'b0;
+                RegWrite = 1'b1;
+                ns = IF;
+            end
+            JumpRcomplete : begin
+                // Please check that this signals are enough to set for this instruction 
+                PCSrc = 2'b11;
+                PCWrite = 1'b1;
+                ns = IF;
+            end
+            JALcomplete : begin
+                WriteRegSel = 1'b1;
+                WriteDataSel = 1'b1;
+                RegWrite = 1'b1;
+                PCSrc = 1'b1;
+                PCWrite = 1'b1;
+                ns = IF;
+            end
+            default : ns = IF;
+        endcase
+    end
+    always @(posedge clk, posedge rst)begin
+        if(rst)
+            ps <= IF;
+        else 
+            ps <= ns;
+    end
+
+endmodule
---
+++ DataPath.v
@@ -0,0 +1,188 @@
+`timescale 1ns/1ns
+
+module MUX32Bit2input (input[31:0]ZEROsel, ONEsel, input selector, output reg [32:0]out);
+    always @(ZEROsel, ONEsel, selector)begin
+        out = 32'b0;
+        if (selector == 1'b0)
+            out = ZEROsel;
+        else if (selector == 1'b1)
+            out = ONEsel;
+    end
+endmodule
+
+module MUX5Bit2input(input [4:0]ZEROsel, ONEsel, input selector, output reg [4:0] out);
+    always @(ZEROsel, ONEsel, selector) begin
+        out = 5'b0;
+        if(selector == 1'b0) 
+            out = ZEROsel;
+        else if (selector == 1'b1)
+            out = ONEsel;
+    end
+endmodule
+
+module MUX32Bit4input (input [31:0]ZEROsel, ONEsel, TWOsel, THREEsel, input [1:0]selector, output reg [31:0]out);
+    always @(ZEROsel, ONEsel, TWOsel, THREEsel, selector)begin
+        out = 32'b0;
+        if (selector == 2'b00)
+            out = ZEROsel;
+        else if (selector == 2'b01)
+            out = ONEsel;
+        else if (selector == 2'b10)
+            out = TWOsel;
+        else if (selector == 2'b11)
+            out = THREEsel;
+    end
+endmodule
+
+module SignExtender(input [15:0]in, output reg [31:0] out);
+    always @(in) begin
+        out = 32'b0;
+        out = 32'(signed'(in));
+    end
+endmodule
+    
+module ZEROExtender(input [25:0]in, output reg [27:0]out);
+    always @(in) begin
+        out = 28'b0000000000000000000000000000; 
+        out = {in, 2'b00};
+    end
+endmodule
+
+module ShiftLeft2bit(input [31:0]in, output reg [31:0]out);
+    always @(in) begin
+        out = 32'b0;
+        out = {in[29:0], 2'b00};
+    end
+endmodule
+
+module ALU32Bit (input [31:0]A, B, input [2:0]ALUop, output reg[31:0] ALUout, output reg ZeroFlag);
+    always @(A, B, ALUop) begin
+        ALUout = 32'b0;
+        ZeroFlag = 1'b0;
+        if (ALUop == 3'b000) // case AND
+            ALUout = A & B;
+        else if (ALUop == 3'b001) // case OR
+            ALUout = A | B;
+        else if (ALUop == 3'b010) // case ADD
+            ALUout = A + B;
+        else if (ALUop == 3'b110) // case SUB
+            ALUout = A - B;
+        else if (ALUop == 3'b111)begin // case SLT
+            if ($signed(A) < $signed(B))  // supposed that A and B are signed check whether comparison happens right?
+                ALUout = 32'b00000000000000000000000000000001;
+            else
+                ALUout = 32'b00000000000000000000000000000000;
+        end
+        if(ALUout == 32'b0)
+            ZeroFlag = 1'b1;
+    end
+endmodule
+
+module Register32BitWithLoad(input [31:0]in, input rst, clk, ldin, output reg[31:0]out);
+    always @(posedge clk, posedge rst) begin
+        if (rst)
+            out <= 32'b0;
+        // Init property has been removed
+        else if (ldin)
+            out <= in;
+    end
+endmodule
+
+module Register32BitWithoutLoad (input [31:0]in, input rst, clk, output reg [31:0]out);
+    always@(posedge clk, posedge rst) begin
+        if (rst)
+            out <= 32'b0;
+        else 
+            out <= in;
+    end
+endmodule
+
+module RegFile (input [4:0]ReadReg1, ReadReg2, input [4:0]WriteReg, input [31:0]Writedata, input clk, rst, regWriteSignal, output reg [31:0]ReadData1, ReadData2);
+    reg [31:0] REGFILE [0:31];
+
+    always @(ReadReg1, ReadReg2) begin
+        {ReadData1, ReadData2} = 64'b0;
+        ReadData1 = REGFILE[ReadReg1];
+        ReadData2 = REGFILE[ReadReg2];
+    end
+    integer i;
+    always @(posedge clk, posedge rst)begin
+        if (rst) begin
+            for(i = 0; i < 32; i = i + 1) begin
+                REGFILE[i] = 32'b0;
+            end
+        end
+        if (regWriteSignal)
+            REGFILE[WriteReg] = Writedata;
+    end
+endmodule
+
+module Memory (input [31:0]Address, WriteData, input MemRead, MemWrite, clk, rst, output reg [31:0]ReadData);
+    reg [31:0] Mem [0:512];
+    always @(negedge rst) begin
+        $readmemb("Memory2.mem", Mem); // Change it to Memory1.mem or Memory2.mem for 1 and 2!
+    end
+    always @(Address, MemRead, negedge rst) begin
+        ReadData = 32'b00000000000000000000000000000000;
+        if (MemRead)
+            ReadData = Mem[Address[31:2]];
+    end
+    integer i;
+    always@( posedge clk, posedge rst) begin
+        if (rst)
+            for(i = 0; i < 512; i = i + 1)begin
+                Mem[i] = 32'b00000000000000000000000000000000;
+            end
+        else if (MemWrite)
+            Mem[Address[31:2]] = WriteData;
+    end
+endmodule
+
+module MIPSDatapath (input PCWrite, PCWriteCond, IorD, MemWrite, MemRead, IRWrite, RegDst, WriteRegSel, MemtoReg, WriteDataSel, RegWrite, ALUSrcA, rst, clk, input [1:0]ALUSrcB, PCSrc, input [2:0]ALUoperation, output ZeroFlag, output [31:0]Instruction);
+    wire PCload, Andout, zeroflagout;
+    wire [31:0] PCWire, AddressWire, DataWire, IRout, MDRout, MemtoRegWire, WDataInput, ReadData1Wire, ReadData2Wire, RegAout, RegBout, container4, SEXTout, Shl2Sextout, SrcAin, SrcBin, mainALUout, ALUoutRegWire, PCinWire, ZeroExtout2;
+    wire [4:0] RegDstWire, container31, WriteRegInput;
+    wire [27:0] ZeroExtout;
+    
+    assign PCload = PCWrite | Andout;
+    assign Andout = PCWriteCond & zeroflagout;
+    assign container31 = 5'b11111; // 5bit 31 value
+    assign container4 = 32'b00000000000000000000000000000100; // 32bit 4 value
+    assign ZeroExtout2 = {PCWire[31:28], ZeroExtout};
+
+    MUX32Bit2input MUX1(PCWire, ALUoutRegWire, IorD, AddressWire);
+    MUX5Bit2input MUX2(IRout[20:16], IRout[15:11], RegDst, RegDstWire);
+    MUX5Bit2input MUX3(RegDstWire, container31, WriteRegSel, WriteRegInput);
+
+    MUX32Bit2input MUX4(ALUoutRegWire, MDRout, MemtoReg, MemtoRegWire);
+    MUX32Bit2input MUX5(MemtoRegWire, PCWire, WriteDataSel, WDataInput);
+    MUX32Bit2input MUX6(PCWire, RegAout, ALUSrcA, SrcAin);
+
+    MUX32Bit4input MUX7(RegBout, container4, SEXTout, Shl2Sextout, ALUSrcB, SrcBin);
+    MUX32Bit4input MUX8(mainALUout, ZeroExtout2, ALUoutRegWire, RegAout, PCSrc, PCinWire);
+
+    SignExtender SignExtend(IRout[15:0], SEXTout);
+    ShiftLeft2bit Shl2sext(SEXTout, Shl2Sextout);
+    ZEROExtender ZeroExt(IRout[25:0], ZeroExtout);
+
+    Register32BitWithLoad PC(PCinWire, rst, clk, PCload, PCWire);
+    Register32BitWithLoad IR(DataWire, rst, clk, IRWrite, IRout);
+
+    Register32BitWithoutLoad MDR(DataWire, rst, clk, MDRout);
+    Register32BitWithoutLoad A(ReadData1Wire, rst, clk, RegAout);
+    Register32BitWithoutLoad B(ReadData2Wire, rst, clk, RegBout);
+    Register32BitWithoutLoad ALUout(mainALUout, rst, clk, ALUoutRegWire);
+
+    ALU32Bit MainALU (SrcAin, SrcBin, ALUoperation, mainALUout, zeroflagout);
+
+    Memory MainMemory(AddressWire, RegBout, MemRead, MemWrite, clk, rst, DataWire);
+    RegFile MainRegFile(IRout[25:21], IRout[20:16], WriteRegInput, WDataInput,  clk, rst, RegWrite, ReadData1Wire, ReadData2Wire);
+
+    // output assignment
+    assign ZeroFlag = zeroflagout;
+    assign Instruction = IRout;
+
+
+endmodule
+
+
